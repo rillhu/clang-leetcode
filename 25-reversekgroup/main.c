a17590fe3bc2d124126566b774514faf8889b780
rillhu <rillhu@126.com>
2018-07-20 00:00:15 +0800

    add new case

--- 25-reversekgroup/main.c
+++ 25-reversekgroup/main.c
@@ -41,55 +41,75 @@ ListNodeT *createList(int *eles, int size) {
  * };
  */
 
-struct ListNode *reverseList(struct ListNode *head, int k) {
-    int cnt = 0;
-    struct ListNode *p = head;
-    while (cnt++ < k && p != NULL) {
-        p = p->next;
-    }
-    if (cnt < k) return head;
-
-
-    struct ListNode *p1, *p2, *p3;
-    p1 = p2 = p3 = head;
-
-    int loop = 0;
-    while (loop < k) {
-        if (p1->next != NULL) {
-            if (loop == 0) {
-                p1 = p1->next;  //First node handling
-            } else {
-                p3 = p1->next;
-                p1->next = p2;
-                p2 = p1;
-                p1 = p3;
-            }
-        }
-        loop++;
-    }
-
-    head->next = p1;
-    return p2;
-}
-
 struct ListNode *reverseKGroup(struct ListNode *head, int k) {
 
-    struct ListNode *ret;
-    while(head!=NULL) {
-        ret = reverseList(head, k);
-        printList(ret);
+    //Add a head for the list
+    struct ListNode *pp, *ppp;
+    pp = ppp = head;
+    int lpOut = 0;
+    while (pp) {
+        /*Check if list has enough node to reverse
+         * if not, directly return the head pointer*/
+        int cnt = 0;
+        struct ListNode *p;
+        p = pp;
+
+        while (cnt < k && p != NULL) {
+            p = p->next;
+            cnt++;
+        }
+        if (cnt < k) return head;   //This will not cause problem
+                                    // since the k is less or equal the list length
+
+
+        /*Reverse the sub-list with len == k*/
+        struct ListNode *p1, *p2, *p3;
+        p1 = p2 = p3 = pp;
+        int lpIn = 0;
+        while (lpIn < k) {
+            if (p1 != NULL) {
+                if (lpIn == 0) {
+                    p1 = p1->next;  //First node handling
+                } else {
+                    p3 = p1->next;
+                    p1->next = p2;
+                    p2 = p1;
+                    p1 = p3;
+                }
+            }
+            lpIn++;
+        }
+
+        /*Post-update the ppp and pp pointers*/
+        if (lpOut == 0) {
+            ppp = pp;
+            ppp->next = p1;
+            pp = p1;
+        } else {
+            ppp->next = p2;
+            pp->next = p1;
+            ppp = pp;
+            pp = p1;
+        }
+
+        /*Only update the head node pointer when first loop*/
+        if (lpOut == 0) {
+            head = p2;
+        }
+
+        lpOut++;    //Update the loop counter
     }
-    return ret;
+    return head;
 }
 
 
 int main() {
-    int list1[] = {1, 2, 3, 4, 5};// 6, 7, 8, 9};
+    int list1[] = {1, 2, 3, 4, 5, 6};// 6, 7, 8, 9};
 
     ListNodeT *l1 = createList(list1, sizeof(list1) / sizeof(int));
-    printList(l1);
+    //printList(l1);
 
-    struct ListNode *exKList = reverseKGroup(l1, 3);
+    struct ListNode *exKList = reverseKGroup(l1, 6);
     printList(exKList);
 
     return 0;
