1dfb8d598f5123b229154ea46db458f721b10e1e
hu <hu@mail.com>
2018-08-02 10:30:11 +0800

    update 56

--- 56-merge/main.c
+++ 56-merge/main.c
@@ -9,6 +9,7 @@
  * };
  */
 /**
+ *
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
@@ -29,6 +30,25 @@ int max(int v1, int v2) {
 struct Interval *merge(struct Interval *intervals, int intervalsSize, int *returnSize) {
     //printf("size: %d\n", intervalsSize);
     if (intervalsSize == 0) return NULL;
+
+    for (int i = 0; i < intervalsSize ; ++i) {
+        for (int j = i+1; j < intervalsSize; ++j) {
+            if(intervals[i].start>intervals[j].start){
+                int t = intervals[i].start;
+                intervals[i].start = intervals[j].start;
+                intervals[j].start = t;
+
+                t = intervals[i].end;
+                intervals[i].end = intervals[j].end;
+                intervals[j].end = t;
+            }
+        }
+    }
+
+//    for (int i = 0; i < intervalsSize; ++i) {
+//        printf("[%d,%d]\n", intervals[i].start, intervals[i].end);
+//    }
+
     struct Interval *res = (struct Interval *) calloc(intervalsSize, sizeof(struct Interval));
 
     int i = 0;
@@ -72,7 +92,11 @@ struct Interval *merge(struct Interval *intervals, int intervalsSize, int *retur
         resId++;
     }
 
-//    *returnSize = resId;
+
+
+
+    *returnSize = resId;
+    return res;
 //    int min = res[0].start;
 //    int max = res[0].end;
 //    int minId = 0, maxId = 0;
@@ -94,60 +118,60 @@ struct Interval *merge(struct Interval *intervals, int intervalsSize, int *retur
 //        res[0] = res[minId];
 //    }
 
-    int id = 0;
-    i = 0;
-    struct Interval *res2 = (struct Interval *) calloc(resId, sizeof(struct Interval));
-    while (i < resId) {
-        if (res[i].start == -1 && res[i].end == 0) { //Invalid interval, using [-1, 0] as the magic word
-            i++;
-            continue;
-        }
-        res2[id] = res[i];
-        for (int j = i + 1; j < resId; ++j) {
-            if (res[j].start == -1 && res[j].end == 0) { //Invalid interval, using [-1, 0] as the magic word
-                continue;
-            }
-            if ((res2[id].end < res[j].start) || (res2[id].start > res[j].end)) {  //No overlap
-
-            } else if (res2[id].end >= res[j].start && res2[id].end <= res[j].end &&
-                       res2[id].start <= res[j].start) { //overlap type 1: [1,3], [2,6]
-                res2[id].end = res[j].end;
-
-                res[j].start = -1;
-                res[j].end = 0;
-            } else if ((res2[id].end >= res[j].start && res2[id].end >= res[j].end &&
-                        res2[id].start >= res[j].start)) {//overlap type 1: [2,6], [1,3]
-                res2[id].start = res[j].start;
-                res[j].start = -1;
-                res[j].end = 0;
-            } else if (res2[id].end >= res[j].start && res2[id].end >= res[j].end &&
-                       res2[id].start <= res[j].start) { //overlap type 2: [1,7], [2,6]
-                res[j].start = -1;
-                res[j].end = 0;
-            } else if (res2[id].end >= res[j].start && res2[id].end <= res[j].end &&
-                       res2[id].start >= res[j].start) { //overlap type 2: [2,6], [1, 7]
-                res2[id].start = res[j].start;
-                res2[id].end = res[j].end;
-                res[j].start = -1;
-                res[j].end = 0;
-            }
-        }
-        i++;
-        id++;
-    }
-    *returnSize = id;
-
-    return res2;
+//    int id = 0;
+//    i = 0;
+//    struct Interval *res2 = (struct Interval *) calloc(resId, sizeof(struct Interval));
+//    while (i < resId) {
+//        if (res[i].start == -1 && res[i].end == 0) { //Invalid interval, using [-1, 0] as the magic word
+//            i++;
+//            continue;
+//        }
+//        res2[id] = res[i];
+//        for (int j = i + 1; j < resId; ++j) {
+//            if (res[j].start == -1 && res[j].end == 0) { //Invalid interval, using [-1, 0] as the magic word
+//                continue;
+//            }
+//            if ((res2[id].end < res[j].start) || (res2[id].start > res[j].end)) {  //No overlap
+//
+//            } else if (res2[id].end >= res[j].start && res2[id].end <= res[j].end &&
+//                       res2[id].start <= res[j].start) { //overlap type 1: [1,3], [2,6]
+//                res2[id].end = res[j].end;
+//
+//                res[j].start = -1;
+//                res[j].end = 0;
+//            } else if ((res2[id].end >= res[j].start && res2[id].end >= res[j].end &&
+//                        res2[id].start >= res[j].start)) {//overlap type 1: [2,6], [1,3]
+//                res2[id].start = res[j].start;
+//                res[j].start = -1;
+//                res[j].end = 0;
+//            } else if (res2[id].end >= res[j].start && res2[id].end >= res[j].end &&
+//                       res2[id].start <= res[j].start) { //overlap type 2: [1,7], [2,6]
+//                res[j].start = -1;
+//                res[j].end = 0;
+//            } else if (res2[id].end >= res[j].start && res2[id].end <= res[j].end &&
+//                       res2[id].start >= res[j].start) { //overlap type 2: [2,6], [1, 7]
+//                res2[id].start = res[j].start;
+//                res2[id].end = res[j].end;
+//                res[j].start = -1;
+//                res[j].end = 0;
+//            }
+//        }
+//        i++;
+//        id++;
+//    }
+//    *returnSize = id;
+//
+//    return res2;
 }
 
 
 int main() {
-    struct Interval arr[] = {
-            {1,  3},
-            {2,  6},
-            {8,  10},
-            {15, 18}
-    };
+//    struct Interval arr[] = {
+//            {1,  3},
+//            {2,  6},
+//            {8,  10},
+//            {15, 18}
+//    };
 //    struct Interval arr[] = {
 //            {1, 4},
 //            {4, 5}
