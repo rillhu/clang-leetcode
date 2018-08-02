5af799339b385f5e289b42bc1bac119c26e5f990
hu <hu@mail.com>
2018-08-02 09:01:33 +0800

    update readme

---
+++ 36-search/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//输入: nums = [4,5,6,7,0,1,2], target = 0
+//输出: 4
+//示例 2:
+//
+//输入: nums = [4,5,6,7,0,1,2], target = 3
+//输出: -1
+
+int search(int *nums, int numsSize, int target) {
+
+    return 0;
+
+}
+
+int main() {
+    int nums[] = {4, 5, 6, 7, 0, 1, 2};
+
+    printf("%d\n", search(nums, sizeof(nums) / sizeof(int), 0));
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -10,7 +10,9 @@ implementation of leetcode
 - [ ] 15 cannot be submitted due to: time out
 - [ ] 22 is not implemented yet, which require recursive program.
 - [ ] 33 is not finished
+- [ ] 36 is not finished
 - [ ] 38 is not finished
+- [ ] 56is not finished
 
 
 
